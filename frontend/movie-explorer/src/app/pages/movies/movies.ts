3c585f1a1ecc9dc8677604c5eceb8e68be247465
Saksham Gupta <sakshamgupta0155@gmail.com>
2026-09-09 23:55:29 +0530

    s

--- frontend/movie-explorer/src/app/pages/movies/movies.ts
+++ frontend/movie-explorer/src/app/pages/movies/movies.ts
@@ -21,7 +21,7 @@ interface Movie {
   templateUrl: './movies.html'
 })
 export class Movies implements OnInit {
-
+age=0
   private readonly API_URL = 'https://api.tvmaze.com/shows';
 
   // Signals: state that the template reads. Updating these via .set()
